92093594bd8b00e614c0f8ece10c2b0040df7e7a
JukkaJoronen <jukka.joronen@jukitech.fi>
2019-03-27 10:47:13 +0200

    Update flask_start.sh

--- flask_start.sh
+++ flask_start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-source /home/maintenance/jms/virtuaalijms/bin/activate
-export FLASK_APP=~/jms/jms.py
+source /home/jms/JMS/virtuaalijms/bin/activate
+export FLASK_APP=/home/jms//JMS/jms.py
 export FLASK_DEBUG=1
-/home/maintenance/jms/virtuaalijms/bin/flask run --host=192.168.44.111
+/home/jms/JMS/virtuaalijms/bin/flask run --host=192.168.44.111
